456c9e63f4ded12b9ef1de6afe3b48946256fb52
Florentina Aiko <florenta@ev-l1.kzn.21-school.ru>
2023-03-18 13:54:40 +0300

    started game_of_life

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+#define LENGTH 80
+#define HEIGHT 25
+
+void draw(int**a, const int n, const int m);
+void init(int a[][LENGTH]);
+
+
+int main(void) {
+
+    const int x1 = 0;
+    const int y1 = 0;
+
+    int field[height][length];
+
+
+
+    return 0;
+}
